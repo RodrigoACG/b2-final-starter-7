3f5224ee29bd3d0f3253213322e15088a6608f70
Rodrigo Chavez <Rodrigo103004@gmail.com>
2024-04-20 22:42:10 -0600

    Wip

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -3,4 +3,9 @@ require 'rails_helper'
 RSpec.describe Coupon, type: :model do
   it {should belong_to :merchant}
   it {should have_many :invoices}
+  
+
+  # describe '#validations' do
+  #   it { should validate_uniqueness_of :code }
+  # end
 end
