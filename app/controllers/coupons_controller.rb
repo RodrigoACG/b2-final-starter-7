7b7ad4a2c50e9aca623fd57d33233e5cd09391ac
Rodrigo Chavez <Rodrigo103004@gmail.com>
2024-04-20 19:17:50 -0600

    Add new, create and strong params functions in controller

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -6,4 +6,17 @@ class CouponsController < ApplicationController
   def show 
     @coupon = Coupon.find(params[:id])
   end
+
+  def new;end
+
+  def create 
+   Coupon.create!(coupon_params)
+   redirect_to merchant_coupons_path(params[:merchant_id])
+  end
+
+  private 
+
+  def coupon_params
+    params.permit(:name, :code, :dollar_off, :status, :merchant_id)
+  end
 end
