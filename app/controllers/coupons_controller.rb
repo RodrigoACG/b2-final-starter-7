687a36cb0659399b0d1aa73c13c2ee4c4dea8253
Rodrigo Chavez <Rodrigo103004@gmail.com>
2024-04-24 12:01:56 -0600

    modified

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -12,15 +12,6 @@ class CouponsController < ApplicationController
   def create 
     @merchant = Merchant.find(params[:merchant_id])
 
-    # if @merchant.coupons.active.length >= 5 
-    #   flash[:alert] = ("Sorry coupon could not be made at this time, The maximum number of coupons (5) has been reached")
-    # elsif Coupon.exists?(code: coupon_params[:code])
-    #   flash[:alert] = ("Another coupon with this code already exists")
-    # else 
-    #   @merchant.coupons.create!(coupon_params)
-    #   flash[:alert] = ("Coupon successfully added") 
-    # end
-
     if Coupon.exists?(code: coupon_params[:code]) 
       flash[:alert] = ("Another coupon with this code already exists")
     elsif @merchant.coupons.active.count >= 5 
@@ -34,26 +25,6 @@ class CouponsController < ApplicationController
 
   end
 
- 
-
-  # def update 
-  #   merchant = Merchant.find(params[:merchant_id])
-  #   # invoice = Invoice.find(params[:invoice_id])
-
-  #   coupon = Coupon.find(params[:id])
-
-  #   if merchant.invoices.completed
-  #     merchant.coupons.update!(coupon_params)
-  #     flash[:alert] = ("Successfully changed status")
-  #   end
-
-  #   # if merchant.invoices.in_progress
-  #   #   flash[:alert] = ("Sorry, can't deactivate. Coupon is sill in use")
-  #   # end
-
-    
-  #   redirect_to merchant_coupon_path(merchant, coupon)
-  # end
 
   def update
     merchant = Merchant.find(params[:merchant_id])
